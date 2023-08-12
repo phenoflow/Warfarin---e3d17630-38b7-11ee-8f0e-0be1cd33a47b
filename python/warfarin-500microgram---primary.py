338c16a669d1fe5caf8f3654f4174a2ebb5b089e
Martin Chapman <contact@martinchapman.co.uk>
2023-08-12 03:29:51 +0100

    Update made by Phenoflow

---
+++ python/warfarin-500microgram---primary.py
@@ -0,0 +1,25 @@
+# S Jill Stocks, Evangelos Kontopantelis, Artur Akbarov, Sarah Rodgers, Anthony J Avery, Darren M Aschroft, 2023.
+
+import sys, csv, re
+
+codes = [{"code":"40143","system":"gprdproduct"},{"code":"59400","system":"gprdproduct"},{"code":"60589","system":"gprdproduct"},{"code":"6262","system":"gprdproduct"}];
+REQUIRED_CODES = 1;
+with open(sys.argv[1], 'r') as file_in, open('warfarin-potential-cases.csv', 'w', newline='') as file_out:
+    csv_reader = csv.DictReader(file_in)
+    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["warfarin-500microgram---primary-identified"])
+    csv_writer.writeheader();
+    codes_identified = 0;
+    for row in csv_reader:
+        newRow = row.copy();
+        for cell in row:
+            # Iterate cell lists (e.g. codes)
+            for item in re.findall(r'\(([^,]*)\,', row[cell]):
+                if(item in list(map(lambda code: code['code'], codes))): codes_identified+=1;
+                if(codes_identified>=REQUIRED_CODES):
+                    newRow["warfarin-500microgram---primary-identified"] = "CASE";
+                    break;
+            if(codes_identified>=REQUIRED_CODES): break;
+        if(codes_identified<REQUIRED_CODES):
+            newRow["warfarin-500microgram---primary-identified"] = "UNK";
+        codes_identified=0;
+        csv_writer.writerow(newRow)
